48474e844fef62d828850790987f4f08e8702013
Jinnapat Yana <jaja_yana@hotmail.com>
2023-11-04 00:57:28 +0700

    Fix grpc api on docker

--- grpc-client.options.ts
+++ grpc-client.options.ts
@@ -4,7 +4,7 @@ import { join } from 'path';
 export const grpcClientOptions: GrpcOptions = {
   transport: Transport.GRPC,
   options: {
-    url: 'localhost:3001',
+    url: '0.0.0.0:3001',
     package: 'parkingSpace',
     protoPath: join(__dirname, './parking-space/parking-space.proto'),
   },
