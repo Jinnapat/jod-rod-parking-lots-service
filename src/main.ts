d7678b4715c014a7cac6e6e873d38e30d47088bf
Palm394 <palm.nichapat@gmail.com>
2023-11-10 13:51:06 +0700

    Connect with service discovery

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { grpcClientOptions } from 'grpc-client.options';
+import { registerWithEureka } from './helper/eureka-helper'
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.connectMicroservice(grpcClientOptions);
+  registerWithEureka("PARKING-LOTS-SERVICE", 4000)
   await app.startAllMicroservices();
   app.enableCors();
   await app.listen(3000);
